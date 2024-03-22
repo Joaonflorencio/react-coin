4913c0e67e6540ed033320c87cb9aa1dfeb65099
João Neto <jflorencioadv@gmail.com>
2024-03-22 19:14:17 +0100

    Proyecto

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      {/* El contenedor principal de tu barra de navegación */}
+     
       <ul>
-        {/* Lista de enlaces. Puedes estilizarla como prefieras */}
-        <li>
+                <li>
           <Link to="/">Home</Link> {/* Enlace a la página principal */}
         </li>
         <li>
-          <Link to="/favorites">Favorites</Link> {/* Enlace a tus favoritos */}
+          <Link to="/favorites">Favorites</Link> {/* Enlace a los favoritos */}
         </li>
-        {/* Puedes agregar más enlaces según necesites */}
+       
       </ul>
     </nav>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,15 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import Coin from './pages/coin.jsx';
 import Favorites from './pages/favorites.jsx';
-// Añade aquí más importaciones de páginas si las tienes
+
 
 const AppRoutes = () => {
   return (
-    <Routes> {/* Aquí defines tus rutas */}
+    <Routes> {/* Aquí defines las rutas */}
       <Route path="/" element={<Home />} /> {/* Ruta para la página principal */}
       <Route path="/coin/:id" element={<Coin />} /> {/* Ruta para la página de detalles de la criptomoneda */}
       <Route path="/favorites" element={<Favorites />} /> {/* Ruta para la página de criptomonedas favoritas (Bonus) */}
-      {/* Puedes añadir más rutas aquí según sea necesario */}
+      
     </Routes>
   );
 };
